5683da3e937a51e178b18d9c474669189264d5cd
jra200327 <dudanow37@gmail.com>
2026-04-03 23:38:12 +0300

    Added realization for componentStorage

---
+++ src/Ecs/Components/ComponentStorage.cpp
@@ -0,0 +1,86 @@
+#include "ComponentStorage.hpp"
+
+template <typename T>
+ComponentStorage<T>::ComponentStorage(internal::IWorldInternal& w, const int id)
+    : _world(w), _id(id), _count(0)
+{
+    _sparse.resize(50, -1);
+    _dense.resize(50, -1);
+    _data.resize(50);
+}
+
+template <typename T>
+void ComponentStorage<T>::Resize(const int sparseSize, const int dataSize) 
+{
+    int oldSparseSize = _sparse.size();
+    if (oldSparseSize < sparseSize) {
+        _sparse.resize(sparseSize);
+        std::fill_n(_sparse.data() + oldSparseSize,
+        sparseSize - oldSparseSize, -1);
+    }
+    int oldDataSize = _dense.size();
+    if (oldDataSize < dataSize) {
+        _dense.resize(dataSize);
+        _data.resize(dataSize);
+        std::fill_n(_dense.data() + oldDataSize,
+        dataSize - oldDataSize, -1);
+    }
+}
+
+template <typename T>
+T& ComponentStorage<T>::Add(const int entityIid, const T& value)
+{
+    Resize((entityIid / 64 + 1) * 64, _data.size() == _count + 1 ? _data.size() + 64 :
+    _data.size());
+    _data[_count] = value;
+    _dense[_count] = entityIid;
+    _sparse[entityIid] = _count;
+    _count++;
+}
+
+template <typename T>
+void ComponentStorage<T>::Remove(const int entityIid)
+{
+int arrayIndex = _sparse[entityIid];
+int lastEntityIid = _dense[--_count];
+_data[arrayIndex] = _data[_count];
+_dense[arrayIndex] = lastEntityIid;
+_sparse[lastEntityIid] = arrayIndex;
+_sparse[entityIid] = -1;
+}
+
+template <typename T>
+bool ComponentStorage<T>::Has(const int entityIid) const
+{ 
+    return entityIid < _sparse.size() && _sparse[entityIid]!= -1; 
+}
+
+template <typename T>
+T& ComponentStorage<T>::Get(const int entityIid) 
+{ 
+    return _data[_sparse[entityIid]];
+} 
+
+template <typename T>
+const std::span<const T> ComponentStorage<T>::All() const 
+{ 
+    return std::span(_data.begin(), _count); 
+}
+
+template <typename T>
+const std::span<const int> ComponentStorage<T>::Entities() const 
+{ 
+    return std::span(_dense.begin(), _count); 
+}
+
+template <typename T>
+int ComponentStorage<T>::Count() const
+{
+    return _count;
+}
+
+template <typename T>
+int ComponentStorage<T>::Id() const
+{
+    return _id;
+}
